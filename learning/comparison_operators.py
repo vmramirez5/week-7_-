a32d483d12e7bccd0e339b9607202e80fa53a2ff
vmramirez5 <vmramirez5@cps.edu>
2024-10-25 14:58:08 -0500

    comparison_operators.py

--- learning/comparison_operators.py
+++ learning/comparison_operators.py
@@ -12,16 +12,24 @@
 # Append the value of current to the end of the list seconds Please use the list.append() method to do that.
 
 
-seconds = [1.23, 1.45, 1.02]
-current = 1.11
+# seconds = [1.23, 1.45, 1.02]
+# current = 1.11
+# seconds.append(current)
 
-# Remove item 1.45 from seconds.
-seconds = [1.23, 1.45, 1.02, 1.11]
+# print(seconds)
 
+# # Remove item 1.45 from seconds.
+# seconds = [1.23, 1.45, 1.02, 1.11]
+# seconds.remove(1.45)
 
-# Remove items 1.45, 1.02, and 1.11 from seconds.
-seconds = [1.23, 1.45, 1.02, 1.11]
+# print(seconds)
 
+# # Remove items 1.45, 1.02, and 1.11 from seconds.
+# seconds = [1.23, 1.45, 1.02, 1.11]
+# seconds.remove(1.11)
+# seconds.remove(1.45)
+# seconds.remove(1.02)
+# print(seconds)
 
 ################################comparison operators#########################
 #remember....
@@ -37,7 +45,11 @@ seconds = [1.23, 1.45, 1.02, 1.11]
 
 # Comparison Operators Practice  1:
 # Create two variables (num1 and num2) with the following values: 36 and 17. Check if num1 is greater than or equal to num2 and store the result of that comparison in a variable called my_bool
+# num1 = 36
+# num2 = 17
+# my_bool = num1 >= num2
 
+# print(my_bool)
 
 
 # Comparison Operators Practice  2:
@@ -45,24 +57,34 @@ seconds = [1.23, 1.45, 1.02, 1.11]
 # Inside num1, store the result of the square root of 25
 # Inside num2, store the number 5.
 # Check if num1 is equal to num2 and store the result of that comparison in a variable called my_bool.
-
+import math
+# num1 = math.sqrt(25)
+# num2 = 5
+# my_bool = num1 == num2
+# print(my_bool)
 
 # Comparison Operators Practice #3:
 # Create two variables (num1 and num2):
 
 # Inside num1, store the result of 64 x 3
-
-# Inside num2, store the result of 24 x 8
-
-# Check if num1 is different from num2 and store the result of that comparison in a variable called my_bool.
-
+# num1 = 64 * 3
+# # Inside num2, store the result of 24 x 8
+# num2 = 24 * 8
+# # Check if num1 is different from num2 and store the result of that comparison in a variable called my_bool.
+# my_bool = num1 is num2
 
 
 
 #######################comparison operators challenge#####################
 # Challenge: Compare two numbers entered by the user and check if they are equal or not.
 # If they are not equal, print which one is greater.
-
+num1 = int(input("Choose a number: "))
 # Prompt the user for two numbers
-
+num2 = int(input("Choose a second number: "))
 # Check for equality and greater number
+if num1 == num2:
+    print("They are the same.")
+elif num1 > num2:
+    print(str(num1) + " is greater than " + str(num2))
+elif num2 > num1:
+    print(str(num2) + " is greater than " + str(num1))
